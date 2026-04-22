a7357cef7442ce2e4598591af184ac46eef6286b
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-22 17:55:08 -0500

    re

---
+++ src/routes/pool/json_args.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let owner_id = $state("pool.sleet.near");
+	let code_hash = $state("AjD4YJaXgpiRdiArqnzyDi7Bkr1gJms9Z2w7Ev5esTKB");
+</script>
+
+<!-- ============================================ -->
+<!-- ============================================ -->
+
+<!-- DIV_JSON_ARGS -->
+<div>
+	<h3>json-args builder</h3>
+	owner_id:
+	<input type="text" bind:value={owner_id} />
+	code_hash: (only change this if you know what you are doing)
+	<input type="text" bind:value={code_hash} />
+</div>
+
+<!-- ============================================ -->
+<!-- ============================================ -->
+
+<style>
+	input {
+		width: 100%;
+		font-size: 20px;
+	}
+</style>
