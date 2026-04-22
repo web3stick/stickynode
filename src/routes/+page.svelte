c11c8314055b63a1f6b2a60036f9df43916714e1
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-21 22:42:51 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts"></script>
+
 <!-- ============================================ -->
+<!-- ============================================ -->
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<!-- ============================================ -->
+<!-- ============================================ -->
+
+<style></style>
