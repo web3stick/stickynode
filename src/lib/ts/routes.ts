7ccecf53d952342d8d4f8e11c0b5835cb29695dc
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-22 13:38:57 -0500

    re

--- src/lib/ts/routes.ts
+++ src/lib/ts/routes.ts
@@ -2,7 +2,7 @@
 const ROUTES = {
 	home: { path: "/", label: "HOME" },
 	docker: { path: "/docker", label: "RUNNING WITH DOCKER" },
-	config: { path: "/config", label: "config.json" },
+	config: { path: "/config", label: "~/.near/config.json" },
 	near: { path: "/near", label: "~/.near" }
 } as const;
 
